a5018f01910c498962aa69d48b3f9873e26d7424
Alison Haire <alison.haire@sydney.edu.au>
2022-01-12 15:20:25 +1100

    First Commit

--- README.md
+++ README.md
@@ -1,70 +1,93 @@
-# Getting Started with Create React App
+# Basic Sample Hardhat Project
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
 
-## Available Scripts
+Try running some of the following tasks:
 
-In the project directory, you can run:
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
 
-### `npm start`
+Ways to save your NFT SVG
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+1. You can save it in the contrct itself
+2. You can save it as an IPFS CID
+3. You can use web2 (we wouldn't do that though would we!)
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Run contract: npx hardhat
 
-### `npm test`
+> run scripts/run.js  
+> Deploy contract to rinkeby:
+> npx hardhat run scripts/deploy.js --network rinkeby
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Running the front end localy (host on Fleek)
+npm install && npm start
 
-### `npm run build`
+To make this project from scratch:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Dependencies:
+Node installed.
+Metamask, https://metamask.io/download.html
+Moralis Quick server details (or infure/achemy),
+etherscan account (for verifying contract)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Tasks - setup:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+> npx create-react-app my-app-name
+> cd my-app-name
+> npm install --save-dev hardhat
+> npx hardhat (to get a sample project - choose basic project so we get all the folders. Choose no to gitignore and y to dependencies)
 
-### `npm run eject`
+install dependencies:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+> npm install --save-dev @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers @nomiclabs/hardhat-etherscan @openzeppelin/contracts
+> npm install ethers dotenv
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Tasks - development of contract
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+> touch .env
+> add .env to the .gitignore file
+> rm ./contracts/Greeter.sol
+> rm ./scripts/sample-script.js
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+> touch ./contracts/FilecoinNFTHack.sol
+> touch ./scripts/run.js
+> touch ./scripts/deploy.js
 
-## Learn More
+Write solidity contract
+Write run.js and test your contract
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+> npx hardhat run scripts/run.js
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+Tasks - Deployment of Solidity contract
+Sign up for Moralis and get a QuickNode
+Add it to hardhat config
+Sign up for etherscan / get an API key & add it to hardhat.config
+networks: {
+rinkeby: {
+url: process.env.MORALIS_RINKEBY_API_URL,
+accounts: [process.env.METAMASK_RINKEBY_PRIVATE_KEY],
+},
+},
+etherscan: {
+// Your API key for Etherscan
+// Obtain one at https://etherscan.io/
+apiKey: process.env.ETHERSCAN_API_KEY,
+}
 
-### Code Splitting
+> npx hardhat run scripts/deploy.js --network rinkeby
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+Tasks - development of front end
 
-### Analyzing the Bundle Size
+- Build the front end
+- create a utils folder and put the deployed contract ABI json here
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+Tasks - deployment of front end
 
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- Use Fleek.co
--- src/App.js
+++ src/App.js
@@ -1,25 +1,270 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import filecoinNFTHack from "./utils/FilecoinNFTHack.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Constants
+const TWITTER_HANDLE = "developerally";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = "";
+const TOTAL_MINT_COUNT = 50;
+
+const App = () => {
+  const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [name, setName] = useState("");
+  const [tx, setTx] = useState("");
+  const [openseaLink, setOpenSeaLink] = useState("");
+  const [raribleLink, setRaribleLink] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener();
+    } else {
+      console.log("No authorized account found");
+    }
+
+    //make sure on right network
+    // let chainId = await ethereum.request({ method: 'eth_chainId' });
+    // console.log("Connected to chain " + chainId);
+
+    // // String, hex code of the chainId of the Rinkebey test network
+    // const rinkebyChainId = "0x4";
+    // if (chainId !== rinkebyChainId) {
+    //   alert("You are not connected to the Rinkeby Test Network!");
+    // }
+  };
+
+  /*
+   * Implement your connectWallet method here
+   */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          filecoinNFTHack.abi,
+          signer
+        );
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          setOpenSeaLink(
+            `https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+          setRaribleLink(
+            `https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`
+          );
+          // alert(
+          //   `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          // );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderMintUI = () => (
+    <div>
+      <p className="sub-text">
+        <label>Enter Your Name:</label>
+        <input
+          label="Enter Your Name"
+          type="text"
+          value={name}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setName(e.target.value);
+          }}
+        ></input>
+      </p>
+      <button
+        onClick={askContractToMintNft}
+        className="cta-button connect-wallet-button"
+      >
+        Mint NFT
+      </button>
     </div>
   );
-}
+
+  const askContractToMintNft = async () => {
+    console.log("name value", name);
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          filecoinNFTHack.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT(name);
+
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+
+        setTx(`https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+        console.log(
+          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+        );
+        console.log(
+          `See nft on opensea: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+        );
+        console.log(
+          `See it on rarible: https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:{tokenID}`
+        );
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  /*
+   * Added a conditional render! We don't want to show Connect to Wallet if we're already conencted :).
+   */
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {currentAccount === ""
+            ? renderNotConnectedContainer()
+            : renderMintUI()}
+          {}
+          {tx && (
+            <p>
+              <a
+                className="footer-text"
+                href={tx}
+                target="_blank"
+                rel="noreferrer"
+              >
+                See your Transaction on Etherscan
+              </a>
+            </p>
+          )}
+          {openseaLink && (
+            <p>
+              <a
+                className="footer-text"
+                href={openseaLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                See your NFT on OpenSea
+              </a>
+            </p>
+          )}
+          {raribleLink && (
+            <p>
+              <a
+                className="footer-text"
+                href={raribleLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                See your NFT on Rarible
+              </a>
+            </p>
+          )}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
