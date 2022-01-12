f4e92256ad32cf4721ed308926af848245b70910
Alison Haire <alison.haire@sydney.edu.au>
2022-01-12 15:21:01 +1100

    First Commit

---
+++ contracts/FilecoinNFTHack.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol"; //alows for console.logs in a solidity contract"
+
+// We need to import the helper functions from the contract that we copy/pasted.
+import { Base64 } from "./libraries/Base64.sol";
+
+contract FilecoinNFTHack is ERC721URIStorage {
+
+    // OpenZeppelin function to help us keep track of tokenIds.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    uint256 totalnfts;
+
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract. Woah!");
+        totalnfts=10;
+    }
+
+  // better to use chainlink VRF for this
+  function random(string memory input) internal pure returns (uint256) {
+      return uint256(keccak256(abi.encodePacked(input)));
+  }
+
+  function makeAnEpicNFT(string memory name) public {
+    console.log(totalnfts);
+    console.log(_tokenIds.current());
+    require(_tokenIds.current() < totalnfts);
+    uint256 newItemId = _tokenIds.current();
+
+    string memory finalSvg = string(abi.encodePacked(baseSvg, name, "</text></svg>"));
+
+    // Get all the JSON metadata in place and base64 encode it.
+    string memory json = Base64.encode(
+        bytes(
+            string(
+                abi.encodePacked(
+                    '{"name": "',
+                    // We set the title of our NFT as the generated word.
+                    name,
+                    '", "description": "The best NFT Hack of 2020 so far for sure!.", "image": "data:image/svg+xml;base64,',
+                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                    Base64.encode(bytes(finalSvg)),
+                    '"}'
+                )
+            )
+        )
+    );
+
+    // Just like before, we prepend data:application/json;base64, to our data.
+    string memory finalTokenUri = string(
+        abi.encodePacked("data:application/json;base64,", json)
+    );
+
+    console.log("\n--------------------");
+    console.log(finalTokenUri);
+    console.log("--------------------\n");
+
+    _safeMint(msg.sender, newItemId);
+    
+    // Update your URI!!!
+    _setTokenURI(newItemId, finalTokenUri);
+  
+    _tokenIds.increment();
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    emit NewEpicNFTMinted(msg.sender, newItemId);
+  }
+}
---
+++ scripts/run.js
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+// const hre = require("hardhat");
+
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory(
+    "FilecoinNFTHack"
+  );
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+
+  // Call the function will base svg and the name?
+  let txn = await nftContract.makeAnEpicNFT("Ally");
+  // Wait for it to be mined.
+  await txn.wait();
+
+  // Mint another NFT for fun.
+  txn = await nftContract.makeAnEpicNFT("DeveloperAlly");
+  // Wait for it to be mined.
+  await txn.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
