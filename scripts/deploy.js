93733350de013241ac9e34d9d72b4936dc9e37f9
Alison Haire <alison.haire@sydney.edu.au>
2022-01-12 15:51:11 +1100

    changed contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,16 +6,16 @@ const main = async () => {
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
-  // Call the function.
-  let txn = await nftContract.makeAnEpicNFT("DeveloperAlly@Filecoin");
-  // Wait for it to be mined.
-  await txn.wait();
-  console.log("Minted NFT #1");
+  // // Call the function.
+  // let txn = await nftContract.makeAnEpicNFT("DeveloperAlly@Filecoin");
+  // // Wait for it to be mined.
+  // await txn.wait();
+  // console.log("Minted NFT #1");
 
-  txn = await nftContract.makeAnEpicNFT("DeveloperAlly@Filecoin2");
-  // Wait for it to be mined.
-  await txn.wait();
-  console.log("Minted NFT #2");
+  // txn = await nftContract.makeAnEpicNFT("DeveloperAlly@Filecoin2");
+  // // Wait for it to be mined.
+  // await txn.wait();
+  // console.log("Minted NFT #2");
 };
 
 const runMain = async () => {
