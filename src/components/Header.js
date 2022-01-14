7acc61fd50e1c11305c93dd281c33e0229640998
Alison Haire <alison.haire@sydney.edu.au>
2022-01-14 19:37:26 +1100

    add hackathons link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,17 @@ import nftHackLogo from "../assets/nfthack-logo.svg";
 const Header = () => {
   return (
     <>
-      <img
-        alt="NFTHack Logo"
-        style={{ height: "200px" }}
-        src={nftHackLogo}
-      ></img>
+      <a
+        href="https://hackathons.filecoin.io/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          alt="NFTHack Logo"
+          style={{ height: "200px" }}
+          src={nftHackLogo}
+        ></img>
+      </a>
       <p className="header gradient-text">NFTHack NFT Collection</p>
       <p className="sub-text">
         Limited edition! 100 personalised NFTs made by Filecoin for EthGlobal
